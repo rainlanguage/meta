6fae218193f15a171ff194badff2682dcd427b82
rouzwelt <rouzweltt@gmail.com>
2023-04-04 23:19:11 +0000

    lint commands and nix

---
+++ shell.nix
@@ -0,0 +1,70 @@
+let
+    pkgs = import
+        (builtins.fetchTarball {
+            name = "nixos-unstable-2022-09-26";
+            url = "https://github.com/nixos/nixpkgs/archive/b8e83fd7e16529ee331313993508c3bf918f1d57.tar.gz";
+            sha256 = "1a98pgnhdhyg66176i36rcn3rklihy36y9z4176la7pxlzm4khwf";
+        })
+        { };
+
+    local-test = pkgs.writeShellScriptBin "local-test" ''
+        npm test
+    '';
+
+    flush = pkgs.writeShellScriptBin "flush" ''
+        rm -rf dist
+    '';
+
+    flush-all = pkgs.writeShellScriptBin "flush-all" ''
+        flush
+        rm -rf node_modules
+    '';
+
+    ci-test = pkgs.writeShellScriptBin "ci-test" ''
+        flush-all
+        yarn install --ignore-scripts
+        build
+        local-test
+    '';
+
+    build = pkgs.writeShellScriptBin "build" ''
+        npm run build
+    '';
+
+    build-all = pkgs.writeShellScriptBin "build-all" ''
+        flush-all
+        npm install
+        build
+    '';
+
+    lint = pkgs.writeShellScriptBin "lint" ''
+        yarn lint
+    '';
+
+    lint-fix = pkgs.writeShellScriptBin "lint-fix" ''
+        yarn lint-fix
+    '';
+
+    in
+    pkgs.stdenv.mkDerivation {
+        name = "shell";
+        buildInputs = [
+            pkgs.nixpkgs-fmt
+            pkgs.yarn
+            pkgs.nodejs-16_x
+            build
+            build-all
+            local-test
+            ci-test
+            flush
+            flush-all
+            lint
+            lint-fix
+        ];
+
+        shellHook = ''
+            export PATH=$( npm bin ):$PATH
+            # keep it fresh
+            npm install
+        '';
+    }
