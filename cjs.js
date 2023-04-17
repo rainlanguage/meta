1dafc9a66493741d5a7e6e9d960b1338951b175c
rouzwelt <rouzweltt@gmail.com>
2023-04-17 18:54:48 +0000

    test2

--- cjs.js
+++ cjs.js
@@ -1,21 +1,17 @@
 "use strict";
 
-module.exports = require("./dist/cjs/index");
+const _opMetaSchema = require("./schemas/op.meta.schema.json");
+const _contMetaSchema = require("./schemas/contract.meta.schema.json");
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OpMetaSchema = void 0;
-const schema_json_1 = __importDefault(require("./schemas/op.meta.schema.json"));
 /**
  * @public op meta schema
  */
-exports.OpMetaSchema = schema_json_1.default;
+const OpMetaSchema = _opMetaSchema;
 
-exports.ContractMetaSchema = void 0;
-const schema_json_2 = __importDefault(require("./schemas/contract.meta.schema.json"));
 /**
  * @public contract meta schema
  */
-exports.ContractMetaSchema = schema_json_2.default;
+const ContractMetaSchema = _contMetaSchema;
+
+module.exports = require("./dist/cjs/index");
+module.exports = { ContractMetaSchema, OpMetaSchema };
