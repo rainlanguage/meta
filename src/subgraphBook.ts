eb54f38d5d7a6199940319dcbca55c9ece7777b0
rouzwelt <rouzweltt@gmail.com>
2023-04-17 16:20:14 +0000

    update README

--- src/subgraphBook.ts
+++ src/subgraphBook.ts
@@ -1,7 +1,12 @@
 /**
  * @public Known subgraph endpoints as key/value pairs
  */
-export const sgBook: { [network: number | string]: string } = {
+export const sgBook: { [chainId: number | string]: string } = {
+    "0x80001":      "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
+    0x80001:        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
     mumbai:         "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
-    polygon:        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon"
+    maticmum:       "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
+    137:            "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon",
+    polygon:        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon",
+    matic:          "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon"
 };
