462aa5fb8b6ac3fbe8988cffefd1e93131299dba
rouzwelt <rouzweltt@gmail.com>
2023-09-11 19:59:43 +0000

    update subgraph endpoints

--- src/rainSubgraphs.ts
+++ src/rainSubgraphs.ts
@@ -5,8 +5,18 @@ import { ChainId } from "./utils";
  * @public Known Rain subgraph endpoints paired with EVM chain ids
  */
 export const RAIN_SUBGRAPHS = {
-    [ChainId.POLYGON]         : ["https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon"],
-    [ChainId.POLYGON_TESTNET] : ["https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry"],
+    [ChainId.ETHEREUM]: [
+        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-ethereum",
+        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-np-eth"
+    ],
+    [ChainId.POLYGON]: [
+        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-polygon",
+        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-np-matic"
+    ],
+    [ChainId.POLYGON_TESTNET]: [
+        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry",
+        "https://api.thegraph.com/subgraphs/name/rainprotocol/interpreter-registry-np"
+    ],
 } as const;
 
 export type RAIN_SUBGRAPHS = (typeof RAIN_SUBGRAPHS)[keyof typeof RAIN_SUBGRAPHS];
